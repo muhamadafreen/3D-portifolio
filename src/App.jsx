50e0c5b3a819cc3e0f3fceddf91f12dd0395a04c
Muhamad Afreen <muhamadafreen@techfriar.com>
2024-01-12 09:11:39 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Tech,
+  Works,
+  Feedbacks,
+} from "./components";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
